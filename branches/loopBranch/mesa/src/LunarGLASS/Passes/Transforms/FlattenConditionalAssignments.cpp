e015040bef2afada1a5edd2cd12aaf791199530c
michael@lunarg.com <michael@lunarg.com@ef8e4060-2eb2-c653-76b5-35368577a8d7>
2011-04-09 01:05:59 +0000

    loopsBranch: checkin of works in progress for syncing purposes

---
+++ branches/loopBranch/mesa/src/LunarGLASS/Passes/Transforms/FlattenConditionalAssignments.cpp
@@ -0,0 +1,160 @@
+//===- FlattenConditionalAssignments.cpp - Flatten conditional assignments ===//
+//
+// LunarGLASS: An Open Modular Shader Compiler Architecture
+// Copyright (C) 2010-2011 LunarG, Inc.
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; version 2 of the
+// License.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+// 02110-1301, USA.
+//
+//===----------------------------------------------------------------------===//
+//
+// Author: Michael Ilseman, LunarG
+//
+//===----------------------------------------------------------------------===//
+//
+// Flatten conditional assignments into select instructions.
+//
+// if (p)
+//    v = foo;
+// else
+//    v = bar;
+//
+// Becomes something like:
+//
+// %v = select <ty>, i1 p, <ty> foo, <ty> bar
+//
+// This pass may modify the CFG. After all (or zero) conditional assignments
+// have been removed, it will then see if the conditional is blank, and if so,
+// remove it.
+//
+//===----------------------------------------------------------------------===//
+
+
+#include "llvm/BasicBlock.h"
+#include "llvm/Pass.h"
+#include "llvm/Analysis/Dominators.h"
+#include "llvm/Analysis/LoopInfo.h"
+#include "llvm/Support/CFG.h"
+#include "llvm/Support/raw_ostream.h"
+
+#include "LunarGLASSLlvmInterface.h"
+#include "FlattenConditionalAssignments.h"
+
+#include "Passes/Analysis/IdentifyConditionals.h"
+
+using namespace llvm;
+
+namespace  {
+    class FlattenCondAssn : public FunctionPass {
+    public:
+        // Standard pass stuff
+        static char ID;
+        FlattenCondAssn() : FunctionPass(ID) {}
+        virtual bool runOnFunction(Function&);
+        void print(raw_ostream&, const Module* = 0) const;
+        virtual void getAnalysisUsage(AnalysisUsage&) const;
+
+    protected:
+        IdentifyConditionals* idConds;
+        DominatorTree* domTree;
+
+        bool moveCondAssns(const Conditional*);
+        bool removeDeadCode(const Conditional*);
+        bool removeEmptyConditional(const Conditional*);
+    };
+} // end namespace
+
+bool FlattenCondAssn::runOnFunction(Function& F)
+{
+    domTree = &getAnalysis<DominatorTree>();
+    idConds = &getAnalysis<IdentifyConditionals>();
+
+    bool changed = false;
+
+    for (IdentifyConditionals::const_iterator i = idConds->begin(), e = idConds->end(); i != e; ++i) {
+        const Conditional* cond = i->second;
+        BasicBlock* entry = cond->getEntryBlock();
+
+        // See if the then, else, and merge blocks are all dominated by the
+        // entry. If this doesn't hold, we're not interested in it.
+        if ( ! (   domTree->dominates(entry, cond->getThenBlock())
+                && domTree->dominates(entry, cond->getThenBlock())
+                && domTree->dominates(entry, cond->getMergeBlock())))
+            continue;
+
+        // Move all the conditional assignments out, iteratively in case of some
+        // interdependence
+        while (changed |= moveCondAssns(cond));
+
+        // Eliminate any and all remaining dead code in the branches
+        while (changed |= removeDeadCode(cond));
+
+        // Remove empty conditionals.
+        changed |= removeEmptyConditional(cond);
+
+    }
+
+    return changed;
+}
+
+bool FlattenCondAssn::moveCondAssns(const Conditional* cond)
+{
+    BasicBlock* merge = cond->getMergeBlock();
+
+    // For each phi node, try to move the definitions into the merge block
+    for (BasicBlock::iterator i = merge->begin(); PHINode* pn = dyn_cast<PHINode>(i); ++i) {
+        // <do stuff>
+    }
+
+
+    return false;
+}
+
+bool FlattenCondAssn::removeDeadCode(const Conditional* cond)
+{
+    // <do stuff>
+    return false;
+}
+
+bool FlattenCondAssn::removeEmptyConditional(const Conditional* cond)
+{
+    // <do stuff>
+    return false;
+}
+
+void FlattenCondAssn::getAnalysisUsage(AnalysisUsage& AU) const
+{
+    AU.addRequired<DominatorTree>();
+    AU.addRequired<IdentifyConditionals>();
+
+    return;
+}
+
+void FlattenCondAssn::print(raw_ostream&, const Module*) const
+{
+    return;
+}
+
+char FlattenCondAssn::ID = 0;
+INITIALIZE_PASS(FlattenCondAssn,
+                "flatten-conditional-assignments",
+                "Flatten conditional assignments into select instructions",
+                false,  // Whether it preserves the CFG
+                false); // Whether it is an analysis pass
+
+FunctionPass* llvm::createFlattenConditionalAssignmentsPass()
+{
+    return new FlattenCondAssn();
+}
